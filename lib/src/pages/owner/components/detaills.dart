c5b768b7add7849cc184a0f62edf68d5a90e2156
NkITaa <nikita.reicherthackt@gmail.com>
2022-10-07 20:13:11 +0200

    Detaills Screen refactored

--- lib/src/pages/owner/components/detaills.dart
+++ lib/src/pages/owner/components/detaills.dart
@@ -9,6 +9,7 @@ import '../../../modells/budget.dart';
 import '../../../modells/cost.dart';
 import '../../../widget_builder.dart';
 
+// Enables Forecasts & Comparison between Costs & Budget
 class Detaills extends StatefulWidget {
   const Detaills(
       {super.key,
@@ -18,24 +19,42 @@ class Detaills extends StatefulWidget {
       required this.totalBudgets,
       required this.totalCosts});
 
+  // List of all Costs
   final List<Cost>? costs;
-  final DateTime until;
+
+  // List of Budget
   final List<Budget>? budgets;
+
+  // Sum of all Costs
   final List<double> totalCosts;
+
+  // Sum of whole Budget
   final List<double> totalBudgets;
+
+  // Due Date of Budget
+  final DateTime until;
 
   @override
   State<Detaills> createState() => _DetaillsState();
 }
 
 class _DetaillsState extends State<Detaills> {
+  // List of boolean that is altered depending on, wheter the DetaillsColumn Expandable Button is expanded
   List<bool> expanded = [false, false, false, false, false, false];
+
+  // Enables Access to all ProjectController Methods
   ProjectController projectController = Get.find();
-  late List<double> totalCosts = widget.totalCosts;
+
+  // determines whether editing of costs is enabled or disabled
   bool enabled = false;
+
+  // holds currently active costDeadline
   DateTime? costDeadline;
+
+  // holds currently projected isPrice
   double? isPrice;
 
+  // lists all CostTypes with specific value
   late List<TextEditingController> costsController = [
     TextEditingController(
         text:
@@ -57,6 +76,7 @@ class _DetaillsState extends State<Detaills> {
             "${FormatController.relevantCosts(costs: widget.costs, category: Const.costTypes[5], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0}${Const.currency}")
   ];
 
+  // lists all BudgetTypes with specific value
   late List<TextEditingController> budgetsController = [
     TextEditingController(
         text: "${widget.budgets?[0].value ?? 0}${Const.currency}"),
@@ -72,32 +92,53 @@ class _DetaillsState extends State<Detaills> {
         text: "${widget.budgets?[5].value ?? 0}${Const.currency}")
   ];
 
+  // updates Expanded State of ExpandableButton
   updateExpanded({required bool state, required int index}) {
+    // reassigns state of specific ExpandableTile when triggered
     expanded[index] = state;
+
+    // State is State to upgrade values graphically
     setState(() {});
   }
 
+  // updates Costs when costDeadline is altered
   updateCostsController({required DateTime dateTime}) {
+    // costDeadline is reassigned
     costDeadline = dateTime;
+
+    // the new value of the specific costs is written in the specific TextEditingController
     for (int i = 0; i < costsController.length; i++) {
       costsController[i].text =
           "${FormatController.relevantCosts(costs: widget.costs, category: Const.costTypes[i], date: dateTime)?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0}${Const.currency}";
     }
+
+    // the total isPrice is accordingly updated with sum of new specific costs
     isPrice = costsController
         .map((controller) {
           return double.parse(controller.text.replaceAll(Const.currency, ""));
         })
         .toList()
         .fold<double>(0, (a, b) => (a) + b);
+
+    // State is State to upgrade values graphically
     setState(() {});
   }
 
+  // value of isPrice is updated, when projections are entered
   setIsPrice({required double isPrice}) {
+    // value is assigned to classes variable
     this.isPrice = isPrice;
+
+    // State is State to upgrade values graphically
+    setState(() {});
   }
 
+  // value of enabled is updated, when the editing mode is activated
   setEnabled({required bool enabled}) {
+    // value is assigned to classes variable
     this.enabled = enabled;
+
+    // State is State to upgrade values graphically
     setState(() {});
   }
 
@@ -119,6 +160,10 @@ class _DetaillsState extends State<Detaills> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    /// Builds the Columns
+                    ///
+                    /// * First: Cost Column
+                    /// * Secondly: Budget Column
                     OwnerBuilder.detaillsColumn(
                       costDeadline: costDeadline,
                       updateCostsController: updateCostsController,
@@ -143,9 +188,12 @@ class _DetaillsState extends State<Detaills> {
                     )
                   ],
                 ),
+
+                // Comparison that shows forecasted values in Comparison with Budget
                 OwnerBuilder.buildComparison(
                   redirect: false,
-                  isPrice: isPrice ?? totalCosts.fold(0, (a, b) => a + b),
+                  isPrice:
+                      isPrice ?? widget.totalCosts.fold(0, (a, b) => a + b),
                   shouldPrice: widget.totalBudgets.fold(0, (a, b) => a + b),
                 )
               ],
