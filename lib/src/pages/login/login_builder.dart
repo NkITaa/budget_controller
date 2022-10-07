bbbcfdc80093963b25eb7008c183cac9e0fcdd7a
NkITaa <nikita.reicherthackt@gmail.com>
2022-10-07 20:33:49 +0200

    added Comments to Login Screen

--- lib/src/pages/login/login_builder.dart
+++ lib/src/pages/login/login_builder.dart
@@ -3,25 +3,39 @@ import 'package:flutter/material.dart';
 import '../../const.dart';
 
 class LoginBuilder {
+  // specific Login textFormField
   static Widget textFormField({
     required TextEditingController controller,
     required String hint,
     bool? isPassword,
   }) {
+    /// Assigns bools in Constructor to new Variable
+    ///
+    /// * when the bool is defined it is set to the Constructors value
+    /// * when the bool is undefined it is set to false
     bool password = isPassword ?? false;
     return SizedBox(
       height: 60,
       child: TextFormField(
+        // passes specific Controller to TextField
         controller: controller,
+
+        /// alters obscure text
+        ///
+        /// * password == false -> Text normally shown
+        /// * password == true -> Text obscured
         obscureText: password,
-        enableSuggestions: false,
-        autocorrect: false,
-        cursorColor: const Color(0xff7434E6),
-        style: const TextStyle(color: Color(0xff7434E6)),
+
+        // validates input
         validator: (value) {
+          /// when no String is inputted an error is thrown
+          ///
+          /// * password == false -> nullFieldError
+          /// * password == true -> nullPasswordError
           if (value == "") {
             return password ? Const.nullPasswordError : Const.nullFieldError;
           } else {
+            // when the password is smaller than six chars -> lengthError
             if (value!.length < 6 && password) {
               return Const.lengthError;
             } else {
@@ -29,6 +43,14 @@ class LoginBuilder {
             }
           }
         },
+
+        // suggestions & autocorrect disabled
+        enableSuggestions: false,
+        autocorrect: false,
+
+        // style
+        cursorColor: const Color(0xff7434E6),
+        style: const TextStyle(color: Color(0xff7434E6)),
         decoration: InputDecoration(
             helperText: "",
             contentPadding: const EdgeInsets.only(left: 20),
