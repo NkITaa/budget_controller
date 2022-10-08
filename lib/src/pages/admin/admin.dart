f6606cc071a27da04de4c7312b6a8e861cae0b36
NkITaa <nikita.reicherthackt@gmail.com>
2022-10-08 08:32:08 +0200

    admin skeleton page comment added

--- lib/src/pages/admin/admin.dart
+++ lib/src/pages/admin/admin.dart
@@ -8,25 +8,41 @@ import '../../modells/log.dart';
 import '../../modells/user.dart';
 import 'const_admin.dart';
 
+// skeleton of Admin Page
 class Admin extends StatefulWidget {
   const Admin({super.key, required this.user});
+
+  // current User Object
   final CustomUser user;
   @override
   State<Admin> createState() => _AdminState();
 }
 
 class _AdminState extends State<Admin> {
+  // enables access to all methods of the userController
   UserController userController = Get.find();
+
+  // gives info on whether a log has been read or not
   List<bool?> tileState = [];
+
+  // says whether the logHistory is depicted or not
   bool logHistory = false;
 
+  // enables child-widget to update the value of the logHistory
   setLogHistory({required bool value}) {
+    // updates the logHistories value
     logHistory = value;
+
+    // sets state so the changement can be seen
     setState(() {});
   }
 
+  // enables child-widget to update the value of a tileState
   setTileState({required bool value, required int index}) {
+    // updates the specific tileState value
     tileState[index] = value;
+
+    // sets state so the changement can be seen
     setState(() {});
   }
 
@@ -45,11 +61,20 @@ class _AdminState extends State<Admin> {
             const SizedBox(
               height: 20,
             ),
+
+            /// Header of the page consisting of
+            ///
+            /// * title
             Center(
                 child: Text(
               logHistory ? CAdmin.logHistory : CAdmin.logs,
               style: const TextStyle(fontSize: 28, color: Colors.black),
             )),
+
+            /// Body of the page consists of a row with:
+            ///
+            /// * an IconBar, that enables all the admins actions
+            /// * a ListView that builds all the relevant logs
             Flexible(
               child: Row(
                 children: [
@@ -66,13 +91,18 @@ class _AdminState extends State<Admin> {
                           const SizedBox(
                             height: 20,
                           ),
+
+                          // is responsible for handling the future logs
                           FutureBuilder(
                             future: LogController.loadLogs(),
                             builder: (BuildContext context, snapshot) {
+                              // Shows ErrorText when Snapshot has an error
                               if (snapshot.hasError) {
                                 return CustomBuilder.defaultFutureError(
                                     error: snapshot.error.toString());
                               }
+
+                              // Shows ProgressIndicator during loading
                               if (snapshot.connectionState ==
                                   ConnectionState.waiting) {
                                 return SizedBox(
@@ -83,6 +113,11 @@ class _AdminState extends State<Admin> {
                               }
 
                               final logs = snapshot.requireData;
+
+                              /// build the logs in a ListView
+                              ///
+                              /// * logHistory == true -> all Logs are shown
+                              /// * logHistory == false -> only the relevant logs are shown
                               return SizedBox(
                                 height:
                                     MediaQuery.of(context).size.height - 170,
