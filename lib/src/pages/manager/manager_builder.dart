4f82678521499cb11b1cbeb9e49abb3673e7b7a2
NkITaa <nikita.reicherthackt@gmail.com>
2022-11-03 13:11:28 +0100

    fixed overstep error 2

--- lib/src/pages/manager/manager_builder.dart
+++ lib/src/pages/manager/manager_builder.dart
@@ -80,19 +80,19 @@ class ManagerBuilder {
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[0].value) : (project.costs?[0].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[0].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[0], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[0]),
                   ManagerBuilder.costFieldBuilder(
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[1].value) : (project.costs?[1].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[1].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[1], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[1]),
                   ManagerBuilder.costFieldBuilder(
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[2].value) : (project.costs?[2].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[2].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[2], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[2]),
                 ],
               ),
@@ -105,7 +105,7 @@ class ManagerBuilder {
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[3].value) : (project.costs?[3].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[3].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[3], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[3]),
                 ],
               ),
@@ -139,13 +139,13 @@ class ManagerBuilder {
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[4].value) : (project.costs?[4].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[4].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[4], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[4]),
                   ManagerBuilder.costFieldBuilder(
                       enabled: false,
                       controller: TextEditingController(
                           text:
-                              "${isBuggetSuggested ? (project.budgets?[5].value) : (project.costs?[5].value)}${Const.currency}"),
+                              "${isBuggetSuggested ? (project.budgets?[5].value) : (FormatController.relevantCosts(costs: project.costs, category: Const.costTypes[5], date: DateTime.now())?.fold<double>(0, (a, b) => a + (b?.value ?? 0)) ?? 0)}${Const.currency}"),
                       overlineText: Const.costTypes[5]),
                 ],
               ),
